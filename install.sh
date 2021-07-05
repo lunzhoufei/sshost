738404b0b46b670952c509a2671baea2ca301df7
xiuzhu <xiuzhu@pinduoduo.com>
2021-07-05 09:28:32 +0800

    update get iphost cmd

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ sudo apt install shadowsocks
 sudo ./bbr.sh
 
 # only for ubuntu on vultr.com
-hostip=`ifconfig ens3 |grep "inet " |awk '{print $2}'`
+# hostip=`ifconfig ens3 |grep "inet " |awk '{print $2}'`
+
+
+hostip=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 sed -i "s/8.8.8.8/$hostip/g" shadowsocks.json
 
 # start server
